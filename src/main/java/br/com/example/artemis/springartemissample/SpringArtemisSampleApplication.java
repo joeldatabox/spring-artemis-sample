ee0ad3f53a6ac9198b5b1313474981fc7f2f02f5
Joel Rodrigues Moreira <joel.databox@gmail.com>
2021-08-13 11:27:34 -0300

    commit initial

---
+++ src/main/java/br/com/example/artemis/springartemissample/SpringArtemisSampleApplication.java
@@ -0,0 +1,13 @@
+package br.com.example.artemis.springartemissample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringArtemisSampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringArtemisSampleApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/example/artemis/springartemissample/SpringArtemisSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.example.artemis.springartemissample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringArtemisSampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
